166b406ddf8b129de65dd21318fa1495c800771c
Pratham G Pawar <130501173+pratham07816@users.noreply.github.com>
2026-01-21 18:04:43 +0530

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -55,7 +55,7 @@ const login = [
         return res.status(400).json({ message: 'Invalid credentials' });
       }
 
-      const token = jwt.sign({ id: user._id, email: user.email }, process.env.JWT_SECRET, { expiresIn: '1h' });
+      const token = jwt.sign({ id: user._id, email: user.email }, process.env.JWT_SECRET, { expiresIn: '7d' });
       res.json({ token, user: { id: user._id, name: user.name, email: user.email } });
     } catch (error) {
       res.status(500).json({ message: 'Server error' });
@@ -65,3 +65,4 @@ const login = [
 
 
 module.exports = { register, login };
+
